3de74bfef33c3cb28ce270a9497619ba300d3c41
MartinezChavira <65751427+JesusMTZ13@users.noreply.github.com>
2021-03-05 00:10:14 -0700

    Initial commit

---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('contenido')
+<h1>Estas en usuarios</h1>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+});
+
+Route::get('/profile', function(){
+    return view('profile');
+
+});
+Route::get('/profile/editar', function(){
+    return "Estas editando";
+
+});
+
+
+Route::get('/ver/{id}/{email}', function($id,$email){
+    return "Estas viendo el perfil numero " .$id." con el email " .$email;
+
+});
+
+Route::group(['prefix'=>'admin', 'as'=> 'admin'] ,function(){
+
+    Route::get ('/',function(){ return view('admin') ; });
+    Route::get ('/usuarios',function(){ return view('usuarios') ; });
+   
+});
